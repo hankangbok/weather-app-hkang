62de55172f86438efb1bd54c0797a95a02322da4
Hannah Kang <kyouyatamax@gmail.com>
2018-12-04 20:32:09 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+class JustTesting extends React.Component {
+  render() {
+    return <h1>THIS IS A TEST</h1>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<JustTesting />, document.getElementById('root'));
+console.log("It's looking like weather time");
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
