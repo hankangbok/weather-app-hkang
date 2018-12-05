ea862d534fee1b8794c0abdbd39a8842310ee0ee
Hannah Kang <kyouyatamax@gmail.com>
2018-12-04 21:18:04 -0800

    laying out the structure/elements of the page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-class JustTesting extends React.Component {
+
+
+class WeatherDisplay extends React.Component {
   render() {
-    return <h1>THIS IS A TEST</h1>;
+    return (
+      <div>
+        <h2>This is where the returned data from the weather API will go</h2>
+      </div>
+    );
   }
 }
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<JustTesting />, document.getElementById('root'));
+class LocationEntry extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Please enter a city" />
+        <button type="button"> Enter the location for the weather you want</button>
+        <WeatherDisplay />
+      </div>
+    );
+  }
+}
+
+class PrettyHeader extends React.Component {
+  render() {
+    return (
+      <header>Welcome to the Weather Page!</header> 
+    );
+  }
+}
+
+class TopLevelWeatherApp extends React.Component {
+  render() {
+    return (
+      <div className="top-level-weather-app">
+        <PrettyHeader /> 
+        <h1>This is the top level of the Weather App</h1>
+        <LocationEntry />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render((
+  <TopLevelWeatherApp />
+  ), document.getElementById('root'));
+
+// ReactDOM.render(<JustTesting />, document.getElementById('root'));
 console.log("It's looking like weather time");
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
